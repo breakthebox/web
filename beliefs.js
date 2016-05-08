2ad0635171fe4f92c4546ea3633b0ebe810ef71b
Oliver Schmid <info@olischmid.ch>
2016-05-08 11:40:24 +0200

    improvements for responsive design, new projects and work on some text elements

--- beliefs.js
+++ beliefs.js
@@ -1,38 +1,38 @@
 var beliefs = [
     {
         "avatar_belief": "https://s.gravatar.com/avatar/aa705336ad39eb523f22e883ebee1dc8?s=80",
-        "belief": "Wir glauben an agile Methoden...",
+        "belief": "Wir verwenden agile Methoden...",
         "avatar_reason": "https://s.gravatar.com/avatar/7d69ac318121023d70962f81f266e7e2?s=80",
         "reason": "... weil die meisten Problemstellungen zu komplex sind um sie vollständig planen zu können. "
     },
     {
         "avatar_belief": "https://s.gravatar.com/avatar/7d69ac318121023d70962f81f266e7e2?s=80",
-        "belief": "Wir glauben an offene Standards...",
+        "belief": "Wir nutzen offene Standards...",
         "avatar_reason": "https://s.gravatar.com/avatar/aa705336ad39eb523f22e883ebee1dc8?s=80",
         "reason": "... weil Innovation immer auf Bestehendem aufbaut und offene Standards die beste Grundlage für neue Ideen bilden."
     },
     {
         "avatar_belief": "https://s.gravatar.com/avatar/aa705336ad39eb523f22e883ebee1dc8?s=80",
-        "belief": "Wir glauben an Interdisziplinarität...",
+        "belief": "Wir arbeiten interdisziplinär...",
         "avatar_reason": "https://s.gravatar.com/avatar/7d69ac318121023d70962f81f266e7e2?s=80",
         "reason": "... weil nur dann ganzheitliche Ideen entstehen können, wenn von Beginn weg verschiedene Aspekte berücksichtigt werden."
     },
     {
         "avatar_belief": "https://s.gravatar.com/avatar/7d69ac318121023d70962f81f266e7e2?s=80",
-        "belief": "Wir glauben an \"Self-Enabling\"...",
+        "belief": "Unser Ziel ist das \"Self-Enabling\"...",
         "avatar_reason": "https://s.gravatar.com/avatar/aa705336ad39eb523f22e883ebee1dc8?s=80",
         "reason": "... weil Nachhaltigkeit nur dann entstehen kann, wenn unsere Kunden befähigt werden unsere Dienste mittelfristig selbst sicherzustellen."
     },
 
     {
         "avatar_belief": "https://s.gravatar.com/avatar/aa705336ad39eb523f22e883ebee1dc8?s=80",
-        "belief": "Wir glauben an pragmatische Lösungen...",
+        "belief": "Wir suchen pragmatische Lösungen...",
         "avatar_reason": "https://s.gravatar.com/avatar/7d69ac318121023d70962f81f266e7e2?s=80",
-        "reason": "... weil wir nicht in einer idealen Welt leben und sich theoretische Konstrukte nur selten 1:1 in die Realität umsetzen lassen. Daher machen wir das Beste aus den bestehenden Bedingungen."
+        "reason": "... weil sich theoretische Konstrukte nur selten 1:1 in die Realität umsetzen lassen. Daher machen wir das Beste aus den bestehenden Bedingungen."
     },
     {
         "avatar_belief": "https://s.gravatar.com/avatar/7d69ac318121023d70962f81f266e7e2?s=80",
-        "belief": "Wir glauben an Facilitation...",
+        "belief": "Wir sind überzeugt von Facilitation...",
         "avatar_reason": "https://s.gravatar.com/avatar/aa705336ad39eb523f22e883ebee1dc8?s=80",
         "reason": "... weil die Lösung manchmal in der Luft liegt und nur noch eingefangen werden muss."
     },
