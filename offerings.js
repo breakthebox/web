48fd97319dd75cfad2e75909b238adb81ce13199
Oliver Schmid <oli@breakthebox.ch>
2016-08-19 20:27:47 +0200

    first version of new structure of we offer. Additionally, separated the different parts of the index page to have a better overview

---
+++ offerings.js
@@ -0,0 +1,48 @@
+
+
+$("#description").on("swipeleft",function(event){nextOffering()});
+$("#description").on("swiperight",function(event){previousOffering()});
+var offerings = $("#portfolioTexts").children();
+
+var i = 0;
+
+var nextOffering = function(){
+    hideOffering();
+    if(i<offerings.length-1){
+        i++;
+    }
+    else{
+        i=0;
+    }
+    showOffering();
+};
+
+var previousOffering = function(){
+    hideOffering();
+    if(i>0){
+        i--;
+    }
+    else{
+        i=offerings.length-1;
+    }
+    showOffering();
+};
+
+var setOffering = function(offering){
+    hideOffering();
+    i = offering;
+    showOffering();
+};
+
+var hideOffering = function(){
+    $(offerings[i]).removeClass("active");
+    $(".portfolioDiscipline").removeClass("active");
+};
+
+var showOffering = function(){
+    var d = $("#discipline"+i);
+    if(d.length){
+        d.addClass("active");
+    }
+    $(offerings[i]).addClass("active");
+};
