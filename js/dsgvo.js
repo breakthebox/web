6988d3ef2341d38975529529c3be926b96f3007b
Brigitte Hulliger <brigitte.hulliger@gmail.com>
2019-07-06 19:06:37 +0200

    initialized new structure

---
+++ js/dsgvo.js
@@ -0,0 +1,7 @@
+  window.CookieHinweis_options = {
+  	message: 'Diese Website nutzt Cookies, um bestmögliche Funktionalität bieten zu können.',
+  	agree: 'Ok, verstanden',
+  	learnMore: 'Mehr Infos',
+  	link: 'datenschutz.html', /* Link zu den eigenen Datenschutzbestimmungen */
+  	theme: 'dunkel-unten' /* weitere Theme-Optionen sind dunkel-oben oder hell-unten-rechts */
+  };
